d7decc3adfdc54d5a51110733cc5c9403c2e9fdc
frank <18652171101@163.com>
2018-03-01 00:29:19 +0800

    init

--- client.js
+++ client.js
@@ -1,10 +1,10 @@
 const dgram = require('dgram')
 const PORT = 6000
 const HOST = '47.92.24.241'
-const client = dgram.createSocket('udp4')
 
 module.exports = {
   send (message) {
+    const client = dgram.createSocket('udp4')
     client.send(message, 0, message.length, PORT, HOST, function (err, bytes) {
       if (err) throw err
       console.log('UDP message sent to ' + HOST + ':' + PORT)
