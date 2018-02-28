5f271d9e186167baeb3eab76f7b529de226cbe35
frank <18652171101@163.com>
2018-02-28 14:11:28 +0800

    init

--- cronJob.js
+++ cronJob.js
@@ -4,7 +4,7 @@ const utils = require('./utils')
 const command = require('./command')
 
 const job = new CronJob({
-  cronTime: '10 * * * * *',
+  cronTime: '*/10 * * * * *',
   onTick: function () {
     let ip = utils.getIp()
     command.getConn(function (err, res) {
