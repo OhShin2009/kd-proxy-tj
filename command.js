11489dc4d796e2eddf5a77c1601958c37000356f
frank <18652171101@163.com>
2018-03-09 20:16:25 +0800

    init

--- command.js
+++ command.js
@@ -1,18 +1,40 @@
 const {exec} = require('child_process')
+const fs = require('fs')
+const fse = require('fs-extra')
+const randomString = require('random-string')
 const redis = require('./redis')
 
 const EXP = /(\d+)\sup,\s(\d+)\sconnecting/
+const SECRET_PATH = '/usr/local/etc/ipsec.secrets'
+const DEFAULT_SECRET_PATH = '/usr/local/etc/ipsec.secrets.default'
 
 module.exports = {
 
   register (username, ex, callback) {
-    redis.set(username, 1, 'EX', ex).then(function (res) {
-      if (res === 'OK') {
-        callback(null)
-      } else {
-        callback(new Error('save key failed'))
-      }
-    }).error(callback)
+    redis.get(username).then(res => {
+      if (res) return callback(null, res)
+      let password = randomString({length: 8})
+      let secret = `${username} %any : EAP "${password}"`
+      fs.appendFile(SECRET_PATH, secret, (err) => {
+        if (err) return callback(err)
+        redis.set(username, password).then(function (res) {
+          if (res === 'OK') {
+            callback(null)
+          } else {
+            callback(new Error('save key failed'))
+          }
+        }).error(callback)
+      })
+    })
+  },
+
+  clean () {
+    try {
+      fse.copySync(DEFAULT_SECRET_PATH, SECRET_PATH)
+      redis.flushdb()
+    } catch (err) {
+      console.error(err)
+    }
   },
 
   restart (callback) {
@@ -41,3 +63,4 @@ module.exports = {
     })
   }
 }
+
